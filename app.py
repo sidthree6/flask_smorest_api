8ec1604db17138cc2a36859277bbbec271e9d6a4
Sidd <psiddhu001@gmail.com>
2023-03-16 20:50:04 -0400

    Updated code to make flask-smores compatible

--- app.py
+++ app.py
@@ -1,58 +1,66 @@
+import uuid
 from flask import Flask, request
+from flask_smorest import abort
+from db import items, stores
 
 app = Flask(__name__)
 
-stores = [
-    {
-        'name': 'My Wonderful Store',
-        'items': [
-            {
-                'name': 'Chair',
-                'price': 15.99
-            }
-        ]
-    }
-]
 
 @app.get("/store")
 def get_stores():
-    return {'stores': stores}
+    return {'stores': list(stores.values())}
+
 
 @app.post("/store")
 def create_store():
     # get the request data
-    request_data = request.get_json()
+    store_data = request.get_json()
 
+    store_id = uuid.uuid4().hex
     # create a variable to store the new store
-    new_store = {"name": request_data['name'], "items": []}
+    new_store = {**store_data, "id": store_id}
     # append the new store to the stores list
-    stores.append(new_store)
+    stores[store_id] = new_store
     # return the new store
     return new_store, 201
 
-@app.post("/store/<string:name>/item")
-def create_item(name):
+
+@app.post("/item")
+def create_item():
     # get the request data
-    request_data = request.get_json()
+    item_data = request.get_json()
 
-    for store in stores:
-        if store['name'] == name:
-            new_item = {"name": request_data['name'], "price": request_data['price']}
-            store['items'].append(new_item)
-            return new_item, 201
-    # If store was not found
-    return {"message": "Store not found"}, 404
+    store_id = item_data.get('store_id', None)
 
-@app.get("/store/<string:name>")
-def get_store(name):
-    for store in stores:
-        if store['name'] == name:
-            return store
-    return {"message": "Store not found"}, 404
+    # If store_id is not provided or store_id is not in stores
+    if not store_id or store_id not in stores:
+        abort(404, message="Store not found")
 
-@app.get("/store/<string:name>/item")
-def get_item_in_store(name):
-    for store in stores:
-        if store['name'] == name:
-            return {"items": store['items']}
-    return {"message": "Store not found"}, 404
+    item_id = uuid.uuid4().hex
+    # create a variable to store the new item
+    new_item = {**item_data, "id": item_id}
+    # append the new item to the items list
+    items[item_id] = new_item
+    # return the new item
+    return new_item, 201
+
+
+@app.get("/item")
+def get_all_items():
+    return {"items": list(items.values())}
+
+
+@app.get("/store/<string:store_id>")
+def get_store(store_id):
+    store = stores.get(store_id, None)
+    if store:
+        return store
+    abort(404, message="Store not found")
+
+
+@app.get("/item/<string:item_id>")
+def get_item(item_id):
+    item = items.get(item_id, None)
+    if item:
+        return item
+    abort(404, message="Item not found")
